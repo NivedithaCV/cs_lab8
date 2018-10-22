dac04e3c270e16d61655f5ae7c65d597894e2326
NivedithaCV <42238305+NivedithaCV@users.noreply.github.com>
2018-10-22 07:46:15 -0400

    Add files via upload

---
+++ lab8_q4.cpp
@@ -0,0 +1,15 @@
+//Q.4. Write a program to merge 2 arrays (just one after another into another array), find maximum of 2 arrays [maximum element of all the elements in both the arrays], find minimum of 2 arrays [similar to maximum]. (Use functions for each)
+
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+	//input for size of 1st array	
+	//define 1st array
+	//input for size of 2nd array
+	//define 2nd
+	return 0;
+}
